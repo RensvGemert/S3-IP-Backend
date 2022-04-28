2bd7f8057eee531193ec4cf4419af45a823d0835
Rens Gemert <461328@student.fontys.nl>
2022-04-28 18:16:06 +0200

    Integration test added for ProductController

--- src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
