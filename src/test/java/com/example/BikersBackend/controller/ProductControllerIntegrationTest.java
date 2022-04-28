470152b455dad484b6e5b2169a68a73b33f2ecce
Rens Gemert <461328@student.fontys.nl>
2022-04-28 17:56:28 +0200

    Integration test added for ProductController

--- src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.example.BikersBackend.controller;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,14 +27,9 @@ class ProductControllerIntegrationTest {
         RequestBuilder request = MockMvcRequestBuilders.get("/api/products");
         MvcResult result = mvc.perform(request).andReturn();
 
-        String actual = result.getResponse().getContentAsString();
-        String expected = "[{\"productId\":1,\"productTitle\":\"Fiets 1\",\"productDescription\":\"Test product 1\",\"productPrice\":199.00,\"productImageUrl\":\"https://contents.mediadecathlon.com/p1537948/k$a83fb42f90d368d826fe089b858074a8/sq/racefiets-wielrenfiets-rc500-shimano-sora-met-schijfremmen-grijs.jpg?format=auto&f=969x969\"}]";
+        int actualStatus = result.getResponse().getStatus();
+        int expectedStatus = 200;
 
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void getProductsById(){
+        assertEquals(expectedStatus, actualStatus);
     }
 }
