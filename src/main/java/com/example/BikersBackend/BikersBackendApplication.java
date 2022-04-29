c1989cf4a4044695221ee7f35b8b0c41ede4bf3b
Rens Gemert <461328@student.fontys.nl>
2022-04-29 12:20:47 +0200

    Added API Integration Test for GET products

--- src/main/java/com/example/BikersBackend/BikersBackendApplication.java
+++ src/main/java/com/example/BikersBackend/BikersBackendApplication.java
@@ -2,10 +2,8 @@ package com.example.BikersBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath*:application-context.xml"})
 public class BikersBackendApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
@@ -1,42 +1,34 @@
-//package com.example.BikersBackend.controller;
-//import com.example.BikersBackend.BikersBackendApplication;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//import org.springframework.test.web.servlet.RequestBuilder;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//@Nested
-//@AutoConfigureMockMvc
-//class ProductControllerIntegrationTest {
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    void getAllProducts() throws Exception {
-//
-//        ApplicationContext applicationContext = SpringApplication.run(BikersBackendApplication.class);
-//
-//        var productController = applicationContext.getBean(com.example.BikersBackend.controller.ProductController.class);
-//        productController.getAllProducts();
-//
-//
-//        RequestBuilder request = MockMvcRequestBuilders.get("/api/products");
-//        MvcResult result = mvc.perform(request).andReturn();
-//
-//        int actualStatus = result.getResponse().getStatus();
-//        int expectedStatus = 200;
-//
-//        assertEquals(expectedStatus, actualStatus);
-//    }
-//}
+package com.example.BikersBackend.controller;
+
+import com.example.BikersBackend.BikersBackendApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = BikersBackendApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ProductControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getAllProducts_API_GET_TEST() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/products").accept(MediaType.APPLICATION_JSON)).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+}
