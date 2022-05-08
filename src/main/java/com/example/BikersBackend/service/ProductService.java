5d781f8fccef8991e1e3366f9f9c6dfc056acb70
Rens Gemert <461328@student.fontys.nl>
2022-05-08 15:15:17 +0200

    initial commit

--- src/main/java/com/example/BikersBackend/service/ProductService.java
+++ src/main/java/com/example/BikersBackend/service/ProductService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
