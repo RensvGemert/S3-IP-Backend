670842346857a760597b9c5876626494c2c5b9a0
Rens Gemert <461328@student.fontys.nl>
2022-05-10 14:43:33 +0200

    Delete docker files

--- src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/BikersBackend/controller/ProductControllerIntegrationTest.java
@@ -1,34 +1,34 @@
-//package com.example.BikersBackend.controller;
-//
-//import com.example.BikersBackend.BikersBackendApplication;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(
-//        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-//        classes = BikersBackendApplication.class
-//)
-//@AutoConfigureMockMvc
-//@TestPropertySource(locations = "classpath:application-test.properties")
-//class ProductControllerIntegrationTest {
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Test
-//    public void getAllProducts_API_GET_TEST() throws Exception{
-//        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/products").accept(MediaType.APPLICATION_JSON)).andReturn();
-//        System.out.println(mvcResult.getResponse().getContentAsString());
-//    }
-//}
+package com.example.BikersBackend.controller;
+
+import com.example.BikersBackend.BikersBackendApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = BikersBackendApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ProductControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getAllProducts_API_GET_TEST() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/products").accept(MediaType.APPLICATION_JSON)).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+}
