10d68f73226f9c2c4a07972419aafe5701740d86
Rens Gemert <461328@student.fontys.nl>
2022-05-19 15:18:33 +0200

    Allow origins localhost:8081

--- src/main/java/com/example/BikersBackend/controller/ProductController.java
+++ src/main/java/com/example/BikersBackend/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8081"})
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
--- src/main/java/com/example/BikersBackend/controller/UserController.java
+++ src/main/java/com/example/BikersBackend/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8081"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
